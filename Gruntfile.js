57b92140a6dfbdec3ff40675a3ab112241bedc60
David McClure <david.mcclure@virginia.edu>
2014-01-27 16:46:24 -0800

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80; */
+
+/**
+ * @package     jquery.textinplace
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+
+  });
+
+};
